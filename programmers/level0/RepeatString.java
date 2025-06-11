5828a11d60739b5c36ebc37fe2c501f062ecb7c8
Yongje Kim <yongjekim1009@gmail.com>
2025-06-11 21:16:58 +0900

    문자열 붙여서 출력하기

---
+++ programmers/level0/RepeatString.java
@@ -0,0 +1,13 @@
+// 문자열 붙여서 출력하기
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+        
+        System.out.println(a+b);
+    }
+}
